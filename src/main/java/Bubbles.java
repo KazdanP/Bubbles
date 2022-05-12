050050073a7aa9993be8360ccb3195993ad7016c
KazdanP <Luke.kidd@Hotmail.co.uk>
2022-05-12 11:47:46 +0100

    Initial setup

---
+++ src/main/java/Bubbles.java
@@ -0,0 +1,34 @@
+public class Bubbles {
+
+  public static int[] bubbleBubbles(int[] toSort) {
+
+    int runCounter = 1;
+    boolean swapped;
+
+    for (int j = 0; j <= toSort.length; j++) {
+
+      swapped = false;
+
+      for (int i = 0; i < toSort.length - 1 - j; i++) {
+        int first = toSort[i];
+        int second = toSort[i + 1];
+
+        if (first > second) {
+          toSort[i]  = second;
+          toSort[i + 1] = first;
+          swapped = true;
+        }
+
+        runCounter++;
+      }
+
+      //Checks if nothing has been swapped i.e. already in the correct order
+      if (!swapped) { break; }
+    }
+
+    System.out.println("Times ran: " + runCounter);
+
+    return toSort;
+  }
+
+}
