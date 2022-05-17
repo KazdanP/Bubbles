dbedf920b1527d1904211652bcaef58ae2696edd
KazdanP <Luke.kidd@Hotmail.co.uk>
2022-05-17 15:19:16 +0100

    Built and tested display manager

---
+++ src/main/java/sparta/com/mc/display/DisplayManager.java
@@ -0,0 +1,37 @@
+package sparta.com.mc.display;
+
+import sparta.com.mc.interfaces.AnSorter;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class DisplayManager {
+
+  public static ArrayList<String> getMenuItems() {
+    return new ArrayList<>(Arrays.asList("BubblesSort", "MergerineSort", "TreetSort"));
+  }
+
+  public static void displaySortChoices() {
+    System.out.println("Enter the number of the sorter you wish to choose: ");
+
+    for (String sortType : getMenuItems()) {
+      System.out.println((getMenuItems().indexOf(sortType) + 1) + " - " + sortType);
+    }
+  }
+
+  public static void printBeforeSort(AnSorter sorter, int[] arrayToSort) {
+    System.out.println("Sorting using the " + sorter);
+    System.out.println("Before sorting: \n" + Arrays.toString(arrayToSort));
+  }
+
+  public static void printResult(AnSorter sorter, int[] arrayToSort) {
+    long startTime = System.nanoTime();
+
+    int[] sortedArray = sorter.sort(arrayToSort);
+    long endTime = System.nanoTime();
+
+    System.out.println("Sorted: \n" + Arrays.toString(sortedArray));
+
+    System.out.println("Time taken: " + (endTime - startTime) + " nanoseconds");
+  }
+
+}
---
+++ src/test/java/sparta/com/mc/display/DisplayManagerTest.java
@@ -0,0 +1,33 @@
+package sparta.com.mc.display;
+
+import org.junit.jupiter.api.Test;
+import sparta.com.mc.interfaces.AnSorter;
+import sparta.com.mc.sorters.*;
+
+public class DisplayManagerTest {
+
+  @Test
+  void printBeforeBubbleSortTest() {
+    AnSorter sorter = new BubblesSort();
+    int[] intsIWannaSort = {45, 654, 78, 234, 1, 1, 1, 12312, 43};
+
+    DisplayManager.printBeforeSort(sorter, intsIWannaSort);
+
+    DisplayManager.printResult(sorter, intsIWannaSort);
+  }
+
+  @Test
+  void printBeforeMergeSortTest() {
+    AnSorter sorter = new MergerineSort();
+    int[] intsIWannaSort = {45, 654, 78, 234, 1, 1, 1, 12312, 43};
+
+    DisplayManager.printBeforeSort(sorter, intsIWannaSort);
+
+    DisplayManager.printResult(sorter, intsIWannaSort);
+  }
+
+  @Test
+  void displayChoicesTest() {
+    DisplayManager.displaySortChoices();
+  }
+}
