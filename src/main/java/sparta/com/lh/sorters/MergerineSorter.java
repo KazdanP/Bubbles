8ecc1d05a262bd2b3436f19dda77802abcba18fe
KazdanP <Luke.kidd@Hotmail.co.uk>
2022-05-19 11:37:33 +0100

    Fixed merge sort errors cause by odd length arrays

--- src/main/java/sparta/com/lh/sorters/MergerineSorter.java
+++ src/main/java/sparta/com/lh/sorters/MergerineSorter.java
@@ -2,6 +2,8 @@ package sparta.com.lh.sorters;
 
 import sparta.com.lh.interfaces.AnSorter;
 
+import java.util.Arrays;
+
 public class MergerineSorter implements AnSorter {
 
   @Override
@@ -23,29 +25,33 @@ public class MergerineSorter implements AnSorter {
     return mergeSort(splitArray);
   }
 
-
-  //TODO Rewrite this recursion to work with 1k length array
   private int[] mergeSort(int[][] inArray) {
-
-    SortSorted sorty = new SortSorted();
 
     int arrayLength = inArray.length;
 
     if (arrayLength < 2) { return inArray[0]; }
 
-    if (arrayLength == 3) {
-      int[] anotherArray = sorty.sortTwo(inArray[0], inArray[1]);
-      return sorty.sortTwo(anotherArray, inArray[2]);
-    }
+    int[][] newArray;
 
-    int[][] newArray = new int[arrayLength / 2][];
+    if (arrayLength % 2 == 1) {
+      arrayLength -= 1;
+      int[][] combinedArray = new int[arrayLength][];
+      combinedArray[0] = SortSorted.sortTwo(inArray[0], inArray[1]);
 
-    for (int i = 0, j = 0; i < newArray.length; i++, j+=2) {
-      newArray[i] = sorty.sortTwo(inArray[j], inArray[j+1]);
+      for (int i = 1; i < arrayLength; i++) {
+        combinedArray[i] = inArray[i+1];
+      }
+
+      newArray = combinedArray;
+    } else {
+      newArray = new int[arrayLength / 2][];
+
+      for (int i = 0, j = 0; i < newArray.length; i++, j+=2) {
+        newArray[i] = SortSorted.sortTwo(inArray[j], inArray[j+1]);
+      }
     }
 
     return mergeSort(newArray);
-
   }
 
 }
--- src/main/java/sparta/com/lh/start/SorterGetter.java
+++ src/main/java/sparta/com/lh/start/SorterGetter.java
@@ -1,23 +1,31 @@
 package sparta.com.lh.start;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import sparta.com.lh.exceptions.SorterLoaderException;
 import sparta.com.lh.interfaces.AnSorter;
 import sparta.com.lh.sorters.*;
 
 public class SorterFactory {
 
+  private static final Logger logger = LogManager.getLogger("Sorter Factory");
+
   public static AnSorter getSorter(int choice) throws SorterLoaderException {
+    AnSorter sorter;
     switch (choice) {
       case 1:
+        logger.info("Bubble Sort Selected");
         return new BubblesSorter();
       case 2:
+        logger.info("Merge Sort Selected");
         return new MergerineSorter();
       case 3:
+        logger.info("Binary Sort Tree Selected");
         return new TreetSorter();
       default:
-        throw new SorterLoaderException("Invalid Sorter Choice: " + choice);
+        logger.fatal("Selection Out of Bounds");
+        throw new SorterLoaderException("Invalid Selection: '" + choice + "'");
     }
-
   }
 
 }
